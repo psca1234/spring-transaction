fabbf3505afe63a62e89ce30b6e7018520c9d681
yangjing <psca1234@126.com>
2018-12-22 13:44:31 +0800

    spring transaction复习,添加切面优先级和具体检查业务(解决冲突)

--- src/main/java/com/psca/spring/transaction/aspect/AccountAspect.java
+++ src/main/java/com/psca/spring/transaction/aspect/AccountAspect.java
@@ -1,13 +1,12 @@
 package com.psca.spring.transaction.aspect;
-
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
 import com.psca.spring.transaction.dao.AccountDao;
 import com.psca.spring.transaction.dao.BookDao;
-
+@Order(1)
 @Aspect
 @Component
 public class AccountAspect {
@@ -19,8 +18,11 @@ public class AccountAspect {
 	public void beforeSellBook() {
 		Integer bookPrice = bookDao.showBookPrice();
 		Integer balance = accountDao.showBalance();
+		System.out.println("开始检查账户余额！");
 		if(balance < bookPrice) {
 			throw new RuntimeException("账户余额已经不能购买本书！");
+		}else {
+			System.out.println("账户余额充足，可以购买本书！");
 		}
 	}
 }
